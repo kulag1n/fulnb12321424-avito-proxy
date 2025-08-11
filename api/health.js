8fddeb8cd72e67ddc4ee17def49e534dd015de95
KONSTANTIN <kuulagin@gmail.com>
2025-08-11 14:21:45 +0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,5 +1,4 @@
 // /api/health.js
-
-export default async function handler(req, res) {
-  res.status(200).json({ status: 'OK', timestamp: new Date().toISOString() });
-}
+module.exports = async function handler(req, res) {
+  res.status(200).json({ status: 'OK' });
+};
