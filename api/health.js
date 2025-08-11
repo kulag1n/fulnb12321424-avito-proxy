698426312cbccd3bd3c9273a02f5bdfbea4b860a
KONSTANTIN <kuulagin@gmail.com>
2025-08-11 18:08:25 +0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,4 +1,6 @@
-// /api/health.js
 module.exports = async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
   res.status(200).json({ status: 'OK' });
 };
